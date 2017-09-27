cf94d4b5acd09cf33b8d69b7acec001a65ea1359
kats-hancharova <kats.hancharova@gmail.com>
2017-09-27 14:26:37 +0300

    changes: collectProxies() method added

--- src/main/java/com/account/AccountGenerator.java
+++ src/main/java/com/account/AccountGenerator.java
@@ -8,47 +8,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class AccountGenerator {
 
-    private static final String PASSWORD = "password_1";
-    private static final byte MIN_EMAIL_LENGTH = 6;
-    private static final byte MAX_EMAIL_LENGTH = 30;
-    private static final String FIRST_NAME = "John";
-    private static final String LAST_NAME = "Doe";
-    private static final byte BIRTH_DAY = 10;
-    private static final String BIRTH_MONTH = "May";
-    private static final short BIRTH_YEAR = 1999;
-    private static final String GENDER = "Female";
-    private static final String SIGN_UP_PAGE = "https://accounts.google.com/SignUp";
-
     public static void main(String[] args) throws InterruptedException, IOException {
-        ResourceBundle locations = ResourceBundle.getBundle("config");
-        String driverLocation = locations.getString("driverLocation");
+        ResourceBundle properties = ResourceBundle.getBundle("config");
+        String driverLocation = properties.getString("driverLocation");
 
         System.setProperty("webdriver.gecko.driver", driverLocation);
         WebDriver driver = new FirefoxDriver();
         setUpDriver(driver);
 
-        driver.get("https://free-proxy-list.net/");
-
-        driver.findElement(By.cssSelector(".hx.ui-state-default>select")).click();
-        driver.findElement(By.cssSelector(".hx.ui-state-default>select>[value='yes']")).click();
-        driver.findElement(By.cssSelector("#proxylisttable>tfoot>tr>th:nth-of-type(5)>select")).click();
-        driver.findElement(By.cssSelector("#proxylisttable>tfoot>tr>th:nth-of-type(5)>select>[value='anonymous']")).click();
-
-        Map<String, Integer> mapIpPort = new HashMap();
-
-        int numberOfIPAddresses = driver.findElements(By.cssSelector("#proxylisttable>tbody>tr")).size();
-
-        for (int i = 1; i <= numberOfIPAddresses; i++) {
-            mapIpPort.put(driver.findElement(By.cssSelector("tbody>tr:nth-of-type(" + i + ")>td:nth-of-type(1)")).getText(),
-                    Integer.parseInt(driver.findElement(By.cssSelector("tbody>tr:nth-of-type(" + i + ")>td:nth-of-type(2)")).getText()));
-        }
-
-        driver.close();
+        Map<String, Integer> mapIpPort = collectProxies(driver);
 
         List<String> credentials = new ArrayList();
 
@@ -56,26 +31,26 @@ public class AccountGenerator {
             WebDriver driverProxy = new FirefoxDriver(setUpProxy(pairIpPort.getKey(), pairIpPort.getValue()));
             setUpDriver(driverProxy);
 
-            driverProxy.get(SIGN_UP_PAGE);
-            driverProxy.findElement(By.id("FirstName")).sendKeys(FIRST_NAME);
-            driverProxy.findElement(By.id("LastName")).sendKeys(LAST_NAME);
+            driverProxy.get(properties.getString("signUpPage"));
+            driverProxy.findElement(By.id("FirstName")).sendKeys(properties.getString("firstName"));
+            driverProxy.findElement(By.id("LastName")).sendKeys(properties.getString("lastName"));
 
-            String email = RandomStringUtils.randomAlphanumeric(MIN_EMAIL_LENGTH, MAX_EMAIL_LENGTH);
-
+            String email = RandomStringUtils.randomAlphanumeric(Integer.parseInt(properties.getString("minEmailLength")), Integer.parseInt(properties.getString("maxEmailLength")));
             driverProxy.findElement(By.id("GmailAddress")).sendKeys(email);
-            driverProxy.findElement(By.id("Passwd")).sendKeys(PASSWORD);
-            driverProxy.findElement(By.id("PasswdAgain")).sendKeys(PASSWORD);
+            driverProxy.findElement(By.id("Passwd")).sendKeys(properties.getString("password"));
+            driverProxy.findElement(By.id("PasswdAgain")).sendKeys(properties.getString("password"));
             driverProxy.findElement(By.id("BirthMonth")).click();
 
-            chooseElementFromList(driverProxy, By.cssSelector("[id='BirthMonth'] [class='goog-menuitem-content']"), BIRTH_MONTH);
+            chooseElementFromList(driverProxy, By.cssSelector("[id='BirthMonth'] [class='goog-menuitem-content']"), properties.getString("birthMonth"));
 
-            driverProxy.findElement(By.id("BirthDay")).sendKeys(Byte.toString(BIRTH_DAY));
-            driverProxy.findElement(By.id("BirthYear")).sendKeys(Short.toString(BIRTH_YEAR));
+            driverProxy.findElement(By.id("BirthDay")).sendKeys(properties.getString("birthDay"));
+            driverProxy.findElement(By.id("BirthYear")).sendKeys(properties.getString("birthYear"));
             driverProxy.findElement(By.id("Gender")).click();
 
-            chooseElementFromList(driverProxy, By.cssSelector("[id='Gender'] [class='goog-menuitem-content']"), GENDER);
+            chooseElementFromList(driverProxy, By.cssSelector("[id='Gender'] [class='goog-menuitem-content']"), properties.getString("gender"));
 
             driverProxy.findElement(By.id("submitbutton")).click();
+
             driverProxy.switchTo().activeElement();
 
             if (driverProxy.findElement(By.className("tos-scroll-button-icon")).isDisplayed()) {
@@ -88,8 +63,9 @@ public class AccountGenerator {
                 continue;
             }
             driverProxy.findElement(By.id("iagreebutton")).click();
-//            writeToFile(email, PASSWORD, credentials);
-//            driverProxy.close();
+
+            writeToFile(email, properties.getString("password"), credentials, properties);
+            driverProxy.close();
         }
     }
 
@@ -116,15 +92,35 @@ public class AccountGenerator {
         }
     }
 
-    private static void writeToFile(String email, String password, List<String> credentials) throws IOException {
+    private static void writeToFile(String email, String password, List<String> credentials, ResourceBundle properties) throws IOException {
         credentials.add(email + "@gmail.com," + password);
-        ResourceBundle locations = ResourceBundle.getBundle("config");
-        String credentialsLocation = locations.getString("credentialsLocation");
-//        Files.write(Paths.get(credentialsLocation), credentials);
+        String credentialsLocation = properties.getString("credentialsLocation");
+        Files.write(Paths.get(credentialsLocation), credentials);
     }
 
     private static void setUpDriver(WebDriver driver) {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
+    private static HashMap<String, Integer> collectProxies(WebDriver driver) {
+        driver.get("https://free-proxy-list.net/");
+
+        driver.findElement(By.cssSelector(".hx.ui-state-default>select")).click();
+        driver.findElement(By.cssSelector(".hx.ui-state-default>select>[value='yes']")).click();
+        driver.findElement(By.cssSelector("#proxylisttable>tfoot>tr>th:nth-of-type(5)>select")).click();
+        driver.findElement(By.cssSelector("#proxylisttable>tfoot>tr>th:nth-of-type(5)>select>[value='anonymous']")).click();
+
+        HashMap<String, Integer> mapIpPort = new HashMap();
+        int numberOfIPAddresses = driver.findElements(By.cssSelector("#proxylisttable>tbody>tr")).size();
+
+        for (int i = 1; i <= numberOfIPAddresses; i++) {
+            mapIpPort.put(driver.findElement(By.cssSelector("tbody>tr:nth-of-type(" + i + ")>td:nth-of-type(1)")).getText(),
+                    Integer.parseInt(driver.findElement(By.cssSelector("tbody>tr:nth-of-type(" + i + ")>td:nth-of-type(2)")).getText()));
+        }
+        driver.close();
+
+        return mapIpPort;
+    }
+
 }
